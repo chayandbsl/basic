1f1348f95f98080496c393c13ed32984a9e3cf05
chayandbsl <chayan.dbsl@gmail.com>
2024-09-25 18:21:57 +0600

    done factorial using iterative formula

---
+++ factorial.js
@@ -0,0 +1,32 @@
+/* 
+    ## Requirements:
+    -  Write a function factorial(n) that calculates the factorial of a non-negative integer n
+    ## Constraints:
+    - The function should handle edge cases (like 0!) and should be efficient for the maximum input of 20.
+*/
+
+/**
+ * Calculates the factorial of a non-negative integer n
+ * @param {number} number - positive integer
+ * @returns {number} the factorial of the input number
+ */
+function factorial(number) {
+  let result = 1;
+
+  if (number < 0) {
+    return "Not defined for negative numbers";
+  }
+
+  if (number === 0) {
+    return 1;
+  }
+
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+
+console.log(factorial(-1));
+console.log(factorial(100));
